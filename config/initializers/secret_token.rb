9f60368363bcfcbfd4a6acf01e477507208c84d8
Jumper <queuejumper@gmail.com>
2014-06-24 05:34:54 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '79aae0fcd5e6dd573467fb1c66c92af51b45f1d0bfed86fac28f4f972c468a1db53e8d9dfa2bf60a2286af5e06287838c24a3f1079fde0d39be59903eab3a720'
